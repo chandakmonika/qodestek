4871882c8c069bb49a7c007bb01d53b4a5cec4ec
chandak monika <mchandak688@gmail.com>
2022-05-18 21:56:57 +0530

    1st commit

---
+++ src/Favorite.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Favorite() {
+  return (
+    <>
+
+        <h3>Favorite</h3>
+    </>
+  )
+}
+
+export default Favorite
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Signup from './Signup'
+
+function Header() {
+  return (
+    <>
+    <h3>Header</h3>
+   <Signup />
+    </>
+  )
+}
+
+export default Header
